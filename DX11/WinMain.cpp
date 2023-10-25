65cdb6f0682c4b957a5dcc7019657aaa168ccc26
Harvey Fleming <94860455+Harvey-Fleming@users.noreply.github.com>
2023-10-25 16:46:03 +0100

    Initialised dx3d + Rendering Frames
    Rendering random colour every frame

--- DX11/WinMain.cpp
+++ DX11/WinMain.cpp
@@ -1,90 +1,184 @@
-// include the basic windows header file
+// include the basic windows header files and the Direct3D header files
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+using RGBA = float[4]; //c++11
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+
+// global declarations
+IDXGISwapChain* swapchain;             // the pointer to the swap chain interface
+ID3D11Device* dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;           // the pointer to our Direct3D device context
+ID3D11RenderTargetView* backbuffer;    // global declaration
+
+
+// function prototypes
+void InitD3D(HWND hWnd);    // sets up and initializes Direct3D
+void RenderFrame(void);    // renders a single frame
+void CleanD3D(void);        // closes Direct3D and releases memory
 
 // the WindowProc function prototype
-LRESULT CALLBACK WindowProc(HWND hWnd,
-    UINT message,
-    WPARAM wParam,
-    LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 
 // the entry point for any Windows program
-int WINAPI WinMain(HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-    // the handle for the window, filled by a function
     HWND hWnd;
-    // this struct holds information for the window class
     WNDCLASSEX wc;
 
-    // clear out the window class for use
     ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
-    // fill in the struct with the needed information
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-    wc.lpszClassName = L"WindowClass1";
+    wc.lpszClassName = "WindowClass";
 
-    // register the window class
     RegisterClassEx(&wc);
 
-    // create the window and use the result as the handle
-    hWnd = CreateWindowEx(NULL,
-        L"WindowClass1",    // name of the window class
-        L"Our First Windowed Program",   // title of the window
-        WS_OVERLAPPEDWINDOW,    // window style
-        300,    // x-position of the window
-        300,    // y-position of the window
-        500,    // width of the window
-        400,    // height of the window
-        NULL,    // we have no parent window, NULL
-        NULL,    // we aren't using menus, NULL
-        hInstance,    // application handle
-        NULL);    // used with multiple windows, NULL
+    RECT wr = { 0, 0, 800, 600 };
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-// display the window on the screen
+    hWnd = CreateWindowEx(NULL,
+        "WindowClass",
+        "Our First Direct3D Program",
+        WS_OVERLAPPEDWINDOW,
+        300,
+        300,
+        wr.right - wr.left,
+        wr.bottom - wr.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
     ShowWindow(hWnd, nCmdShow);
+
+    // set up and initialize Direct3D
+    InitD3D(hWnd);
 
     // enter the main loop:
 
-    // this struct holds Windows event messages
     MSG msg;
 
-    // wait for the next message in the queue, store the result in 'msg'
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (TRUE)
     {
-        // translate keystroke messages into the right format
-        TranslateMessage(&msg);
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
 
-        // send the message to the WindowProc function
-        DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                break;
+        }
+        
+        RenderFrame();
     }
 
-    // return this part of the WM_QUIT message to Windows
+    // clean up DirectX and COM
+    CleanD3D();
+
     return msg.wParam;
 }
+
 
 // this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    // sort through and find what code to run for the message given
     switch (message)
     {
-        // this message is read when the window is closed
     case WM_DESTROY:
     {
-        // close the application entirely
         PostQuitMessage(0);
         return 0;
     } break;
     }
 
-    // Handle any messages the switch statement didn't
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+#pragma region D3D
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
+{
+    // create a struct to hold information about the swap chain
+    DXGI_SWAP_CHAIN_DESC scd;
+
+    // clear out the struct for use
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+    // fill the swap chain description struct
+    scd.BufferCount = 1;                                    // one back buffer
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+    scd.OutputWindow = hWnd;                                // the window to be used
+    scd.SampleDesc.Count = 4;                               // how many multisamples
+    scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+    // create a device, device context and swap chain using the information in the scd struct
+    D3D11CreateDeviceAndSwapChain(NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        D3D11_SDK_VERSION,
+        &scd,
+        &swapchain,
+        &dev,
+        NULL,
+        &devcon);
+
+    // get the address of the back buffer
+    ID3D11Texture2D* pBackBuffer;
+    swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // use the back buffer address to create the render target
+    dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+    pBackBuffer->Release();
+
+    // set the render target as the back buffer
+    devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = 800;
+    viewport.Height = 600;
+
+    devcon->RSSetViewports(1, &viewport);
+}
+
+
+// this is the function that cleans up Direct3D and COM
+void CleanD3D(void)
+{
+    // close and release all existing COM objects
+    swapchain->Release();
+    backbuffer->Release();
+    dev->Release();
+    devcon->Release();
+}
+
+// this is the function used to render a single frame
+void RenderFrame(void)
+{
+    // clear the back buffer to a deep blue
+    devcon->ClearRenderTargetView(backbuffer, RGBA((float)(rand() % 10), (float)(rand() % 10), (float)(rand() % 10), 1.0f));
+
+    // do 3D rendering on the back buffer here
+
+    // switch the back buffer and the front buffer
+    swapchain->Present(0, 0);
+}
+#pragma endregion
+
